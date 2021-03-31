56fc7153e3a7a0f9fb5b5738daf2322cd57e57b6
Bao Khang Lam <56993516+khanglb2000@users.noreply.github.com>
2021-03-31 07:57:52 +0700

    Add files via upload

---
+++ J2.S.P0018_StudentRMI/src/student/Student.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author HI
+ */
+public class Student {
+    String email, name;
+    int age;
+    boolean sex;
+
+    public Student(String email, String name, int age, boolean sex) {
+        this.email = email;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Student() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+    
+}
